0b75d25328b8dff5a83c5a5ca25b89ce3879a8d7
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-05-21 00:02:02 -0300

    Add files via upload
    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+/* Integre o React ao seu HTML: Você pode adicionar componentes React interativos em qualquer lugar do seu documento HTML. 
+Para isso, você precisará configurar um ambiente JavaScript que permita o uso da sintaxe JSX
+e divida o seu código em módulos com a sintaxe import/export. */
