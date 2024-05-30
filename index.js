885f144d8788032bd20a95c0e44b64e3a4c02e44
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-05-29 22:25:15 -0300

    Adicionado evento aos Links
    Adicionado transição aos links da seção programa-se

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 /* Integre o React ao seu HTML: Você pode adicionar componentes React interativos em qualquer lugar do seu documento HTML. 
 Para isso, você precisará configurar um ambiente JavaScript que permita o uso da sintaxe JSX
 e divida o seu código em módulos com a sintaxe import/export. */
+const linkAnimado = document.querySelectorAll('.link-programa-se');
+
+linkAnimado.forEach(function(link) {
+    link.addEventListener('mouseover', function() {
+        this.styles.width = '100%';
+    });
+});
